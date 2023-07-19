cdb4bcabc4d9d329b6d6111b56e2ce6dfb3cd373
junoh <amok0316@gmail.com>
2023-07-19 22:21:04 +0900

    로그인 예외처리 완료

--- src/component/BoardSignIn.js
+++ src/component/BoardSignIn.js
@@ -1,39 +1,41 @@
 import { useState } from "react"
 import LoadingModal from "./LoadingModal";
 import axios from "axios";
+import { useForm } from "react-hook-form";
 
 
 const BoardSignIn = () => {
 
     const [loading, setLoading] = useState(false);
-    const [id, setId] = useState();
-    const [password, setPassword] = useState();
+
+    const {register, handleSubmit, watch, formState: {errors}, setFocus} = useForm();
 
 
 
-    const idChangeHandler = (e) =>{
-        setId(e.target.value)
-    }
 
 
-
-    const passwordChangeHandler = (e) =>{
-        setPassword(e.target.value)
-    }
-
-    const submitHandler = async (e) =>{
-        e.preventDefault()
+    const onSubmit = async data =>{
         setLoading(true)
 
         try{
             const body = {
-                'username' : id,
-                'password' : password
+                'username' : data.username,
+                'password' : data.password
             }
             const res = await axios.post('https://fc7oadp240.execute-api.ap-south-1.amazonaws.com/map-app/board/signin', body)
-            console.log(res)
+            console.log(res.status)
+            if(res.status === 200){
+                sessionStorage.setItem('token', res.data.token)
+                sessionStorage.setItem('idx', res.data.idx)
+                sessionStorage.setItem('nickname', res.data.nickname)
+                alert('로그인이 성공하였습니다.')
+            }
         }
-        catch(err){console.log(err)}
+        catch(err)
+        {console.log(err.response.data.success)
+        if(err.response.data.success === false)
+        {alert('로그인에 실패하였습니다.')}
+        }
         finally{setLoading(false)}
     
     }
@@ -42,13 +44,16 @@ const BoardSignIn = () => {
 
     return(<>
 
-    <form onSubmit={submitHandler}>
+    <form onSubmit={handleSubmit(onSubmit)}>
 
 
-        <input type="text" placeholder="ID" onChange={idChangeHandler}></input>
+        <input type="text" placeholder="ID" {...register("username",
+        {required:true})}/>
+        {errors.username?.type === "required" && <p>필수 입력 항목입니다</p>}
 
-        <input type="password"placeholder="password" onChange={passwordChangeHandler}></input>
-
+        <input type="password" placeholder="비밀번호" {...register("password",
+        {required:true})}/>
+        {errors.password?.type === "required" && <p>필수 입력 항목입니다</p>}
 
         <button type="submit">로그인</button>
 
