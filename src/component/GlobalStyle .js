60aabbab876646cb2664aa0bd3ca26cc1e41233a
junoh <amok0316@gmail.com>
2023-07-26 20:58:01 +0900

    v-글꼴적용 테스트

--- src/component/GlobalStyle .js
+++ src/component/GlobalStyle .js
@@ -1,17 +1,18 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'Pretendard Variable';
+    src: local('Pretendard Variable'),
+         url('./font/PretendardVariable.woff2') format('woff2');
+    font-weight: 100 900;
+    font-display: swap;
+  }
+
   body {
-    font-family: 'Noto Sans KR', sans-serif;
+    font-family: 'Pretendard Variable', 'Noto Sans KR', sans-serif;
     word-break: keep-all;
   }
 `;
 
-
-
-
-
-
-
 export default GlobalStyle;
-
